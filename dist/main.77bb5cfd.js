fcb99c173be69e30fa5f52f400aee2bd0a314213
Wadie Benabdouh <wadie.trade@gmail.com>
2023-07-23 01:46:59 +0100

    main

--- dist/main.77bb5cfd.js
+++ dist/main.77bb5cfd.js
@@ -172,7 +172,7 @@ module.exports = reloadCSS;
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"C:\\Users\\wadie\\Desktop\\monogram-clone\\src\\assets\\backgrounds\\modular-background.jpg":[["modular-background.972e8fb7.jpg","assets/backgrounds/modular-background.jpg"],"assets/backgrounds/modular-background.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
